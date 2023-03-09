c593cdca13c68335a8c8c1bd8886681c7035e625
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2023-03-09 16:50:55 +0100

    Add scaffold of the Game class

---
+++ src/game_state/game.py
@@ -0,0 +1,19 @@
+class Game:
+    """Representation of a game."""
+
+    def __init__(self):
+        """Initialize with some configuration maybe?
+
+        Will this have a Board and a Snake properties?
+        """
+        pass
+
+    def handle_events(self):
+        """pygame allows handling events in an easy way.
+
+        So, probably we should have this method and create logic to handle every specific event
+        in different methods. This will only be the dispatcher.
+
+        NOTE: Implement proper Observer pattern in the premium version?
+        """
+        pass
